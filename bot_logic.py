0bb282466e90aa17925a373c722c4ce69bec482e
Kiriya <sami.correggioli@gmail.com>
2024-12-13 08:41:41 +0100

    Update bot_logic.py

--- bot_logic.py
+++ bot_logic.py
@@ -1,4 +1,5 @@
 import random
+from datetime import datetime, date
 
 def gen_pass(pass_length):
     elements = "+-/*!&$#?=@<>"
@@ -8,3 +9,22 @@ def gen_pass(pass_length):
         password += random.choice(elements)
 
     return password
+
+def flip_coin():
+    c = random.randint(0, 1)
+    if c == 0:
+        return "Testa"
+    else:
+        return "Croce"
+
+def gen_emoji():
+    emoji = ["\U0001f642", "\U0001F601", "\U0001F602", "\U0001F914", "\U00002764", "\U0001F389"]
+    return random.choice(emoji)
+
+def current_date():
+    today_date = date.today()
+    return today_date
+
+def current_time():
+    now_time = datetime.now().strftime("%H:%M:%S")
+    return now_time
